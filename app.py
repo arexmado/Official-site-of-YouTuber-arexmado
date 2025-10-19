162e797b00e4536cad5a8a07ea4a327878b1433a
arexmado <arexmado1@gmail.com>
2025-10-19 09:28:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "AREXMADO 서버 정상 작동 중!"
+    return "AREXMADO 공식 Flask 사이트입니다!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
